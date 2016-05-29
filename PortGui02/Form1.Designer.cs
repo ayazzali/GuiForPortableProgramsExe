fcca96b5ee16790e69aaa0ced5784c2f874fef97
ayazzali <ayazzali@mail.ru>
2016-05-30 00:06:16 +0300

    PortGui02Net4(былаЗ)поддержкаХР+расстояяние между иконками
    PortGui02Net4(былаЗ)поддержкаХР+расстояяние между иконками

---
+++ PortGui02/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace PortGui02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+        	this.components = new System.ComponentModel.Container();
+        	System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+        	this.checkBox1 = new System.Windows.Forms.CheckBox();
+        	this.Tray = new System.Windows.Forms.NotifyIcon(this.components);
+        	this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+        	this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+        	this.button1 = new System.Windows.Forms.Button();
+        	this.SuspendLayout();
+        	// 
+        	// checkBox1
+        	// 
+        	this.checkBox1.AutoSize = true;
+        	this.checkBox1.Location = new System.Drawing.Point(1067, 49);
+        	this.checkBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+        	this.checkBox1.Name = "checkBox1";
+        	this.checkBox1.Size = new System.Drawing.Size(455, 21);
+        	this.checkBox1.TabIndex = 0;
+        	this.checkBox1.Text = "если стоит галка, то бдут удаляться программы из этого списка";
+        	this.checkBox1.UseVisualStyleBackColor = true;
+        	// 
+        	// Tray
+        	// 
+        	this.Tray.Icon = ((System.Drawing.Icon)(resources.GetObject("Tray.Icon")));
+        	this.Tray.Text = "notifyIcon1";
+        	this.Tray.Visible = true;
+        	this.Tray.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Tray_MouseClick);
+        	// 
+        	// tableLayoutPanel1
+        	// 
+        	this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+			| System.Windows.Forms.AnchorStyles.Left) 
+			| System.Windows.Forms.AnchorStyles.Right)));
+        	this.tableLayoutPanel1.AutoScroll = true;
+        	this.tableLayoutPanel1.AutoSize = true;
+        	this.tableLayoutPanel1.ColumnCount = 8;
+        	this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 93F));
+        	this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 93F));
+        	this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 93F));
+        	this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 93F));
+        	this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 93F));
+        	this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 93F));
+        	this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 93F));
+        	this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 281F));
+        	this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+        	this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+        	this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+        	this.tableLayoutPanel1.RowCount = 10;
+        	this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+        	this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+        	this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+        	this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+        	this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+        	this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+        	this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+        	this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+        	this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+        	this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+        	this.tableLayoutPanel1.Size = new System.Drawing.Size(940, 292);
+        	this.tableLayoutPanel1.TabIndex = 0;
+        	// 
+        	// button1
+        	// 
+        	this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+        	this.button1.Location = new System.Drawing.Point(1068, 15);
+        	this.button1.Margin = new System.Windows.Forms.Padding(4);
+        	this.button1.Name = "button1";
+        	this.button1.Size = new System.Drawing.Size(111, 28);
+        	this.button1.TabIndex = 1;
+        	this.button1.Text = "button1";
+        	this.button1.UseVisualStyleBackColor = true;
+        	// 
+        	// Form1
+        	// 
+        	this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+        	this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        	this.ClientSize = new System.Drawing.Size(475, 303);
+        	this.Controls.Add(this.button1);
+        	this.Controls.Add(this.checkBox1);
+        	this.Controls.Add(this.tableLayoutPanel1);
+        	this.Cursor = System.Windows.Forms.Cursors.Cross;
+        	this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+        	this.Name = "Form1";
+        	this.Text = "Form1";
+        	this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+        	this.ResumeLayout(false);
+        	this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NotifyIcon Tray;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+
+
+    }
+}
+
---
+++ PortGui02/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+using System.Windows.Input;
+	
+namespace PortGui02
+{
+    public partial class Form1 : Form //, ISerializable
+    {
+        List<string> listExePath = new List<string>();//создаём массив иконок
+        List<PictureBox> Pic = new List<PictureBox>();
+        //Pic[0].Controls.
+        Thread MousePositionThread;
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            try//задаём размеры окна
+            {
+
+                /*
+                  429
+                  807
+                 */
+                StreamReader file = new StreamReader("HeightWidth.txt");
+                this.Height = int.Parse(file.ReadLine());
+                //if (this.Height < 300) this.Height = 429;//когда выкл комп он записывает нулевые (32,142) ширину и высоту
+                this.Width = int.Parse(file.ReadLine()); file.Close();
+                if (this.Width < 500||this.Height < 300) {this.Width = 720;this.Height = 600;}
+                
+            }
+            catch { } ///finally { file.Close(); }
+            //////////////////////////////
+            try
+		            {
+		            string MyDirPath = FoundMyDirPath();//находим папку со всеми прогами
+		            Found_writeToListExePath(MyDirPath);//записываем все пути к Exe-шкам
+		            RemoveUnnecessaryExe(MyDirPath);		            
+		            //////////////////////////////////////////
+		            for (int i = 0; i < listExePath.Count; i++)
+		            {
+		                if (listExePath[i] != null)
+		                {
+		                    System.Drawing.Icon myIcon = System.Drawing.Icon.ExtractAssociatedIcon(listExePath[i]);//иконку проги достаём из пути
+		                   // System.Drawing.Icon ttt=System.Drawing.Icon.ExtractAssociatedIcon(listExePath[i]);
+		                    //	ttt=myIcon;
+		                    Pic.Add(new PictureBox());
+		                    Pic[i].Image = myIcon.ToBitmap();//записываем в очередной PictureBox иконку (предварительно "отконвертировав")
+		                    tableLayoutPanel1.Controls.Add(Pic[i]);//первый столбец ,первая строка
+		                    tableLayoutPanel1.AutoScroll=true;
+		                    Pic[i].Tag = i;//просто i не получается в событиях использовать(оно всегда равно 66)
+		                    Pic[i].AllowDrop = true;
+		                    /////////////////////////////////////////                 
+		                    Pic[i].MouseClick += (b, eArgs) =>//запускаем прогу
+		                    {
+		                    	
+		                        int x = int.Parse((b as PictureBox).Tag.ToString());//находим индекс - альтернатива Pic[i]- не работает
+		                        //ПКМ!, то пусть откроет проводник с прогой!
+		                        if ((eArgs as System.Windows.Forms.MouseEventArgs).Button == System.Windows.Forms.MouseButtons.Right)
+								{
+		                        	Process PrFolder = new Process();
+									ProcessStartInfo psi = new ProcessStartInfo();
+									string file = listExePath[x];  
+									psi.CreateNoWindow = true;
+									psi.WindowStyle = ProcessWindowStyle.Normal;
+									psi.FileName = "explorer";  
+									psi.Arguments = @"/n, /select, " + file;
+									PrFolder.StartInfo = psi;
+									PrFolder.Start();
+								    return;								    
+								}
+		                        if (checkBox1.Checked)
+		                        {
+		                            StreamWriter f = new StreamWriter("RemoveExe.txt", true);//дозапись
+		                            f.WriteLine(listExePath[x].Remove(0, MyDirPath.Length));
+		                            listExePath[x] = null;  //--///--/-/
+		                            Pic[x].Dispose();
+		                            f.Close();
+		                        }
+		                        else
+		                        {
+		                            ProcessStartInfo stInfo = new ProcessStartInfo(listExePath[x]);
+		                            //stInfo.FileName = "C:\\Users\\Public\\Music\\Sample Music\\Kalimba.mp3";
+		                            //stInfo.Verb ="open to exe";
+		                            Process proc = new Process();  //создаем новый процесс
+		                            proc.StartInfo = stInfo;
+		                            proc.Start();//Запускаем процесс
+		
+		                        }
+		                    };
+		                    Pic[i].MouseEnter += (b, e) =>//ПОДСКАЗКИ при наведении
+		                    {
+		                        int x = int.Parse((b as PictureBox).Tag.ToString());//находим индекс - альтернатива Pic[i]- не работает
+		                        toolTip1.SetToolTip(Pic[x], listExePath[x].Remove(0, listExePath[x].LastIndexOf("\\") + 1));//последнее имя папки EXE, +1 чтоб лишних знаков небыло ("\")
+		                    };
+		                    /////////////////////////////////////////////////////////////////////////////////                                
+		                    Pic[i].DragEnter += (b, e) =>//Разрешаем Drop только файлам
+		                    {
+		                        e.Effect = e.Data.GetDataPresent(DataFormats.FileDrop) ?
+		                           DragDropEffects.All : DragDropEffects.None;
+		                    };
+		                    Pic[i].DragDrop += (object b, DragEventArgs e) =>//Извлекаем имя перетаскиваемого файла
+		                    {
+		                        string[] strings = (string[])e.Data.GetData(DataFormats.FileDrop, true);
+		
+		                        int x = int.Parse((b as PictureBox).Tag.ToString());//находим индекс - альтернатива Pic[i]- не работает
+		
+		                        Process.Start(listExePath[x], strings[0]);
+		                    };
+		                }
+		                else Pic.Add(null);
+		            }
+		
+		            button1.Click += (object sender, EventArgs e) =>
+		        {
+		            if (MousePositionThread != null)
+		            {
+		                MousePositionThread.Abort();
+		                MousePositionThread = null;
+		            }
+		            else
+		            {
+		                MousePositionThread = new Thread((send) =>
+		                {//Считывает координаты мышки в кнопку справа(скрыта поумолчанию)
+		                    while (true)
+		                    {
+		                        button1.Text = Form1.MousePosition.ToString();
+		                    }
+		                });
+		                MousePositionThread.Start();
+		            }
+		            };
+        }
+        catch(Exception Ex) {MessageBox.Show(Ex.Message);}/**/
+        }/*
+        private string MousePositionToString()
+        {
+            while (true)
+            {
+                button1.Text = Form1.MousePosition.ToString();
+            }
+        }/**/
+        private string FoundMyDirPath()
+        {
+            string MyDirPath = "myFavoritePrograms";//как называется папку с программами?
+            string firstItemOfDir=null;
+				
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (var disc in drives)
+            {
+                DirectoryInfo dir = new DirectoryInfo(disc.ToString());  
+                //dir.Extension.//ищем полный путь к папке с портабельными прогами и записываем её в MyDirPath
+                try
+                {
+                    foreach (var item in dir.GetDirectories())
+                        if (item.Name == MyDirPath)
+	                    {
+                    		if(firstItemOfDir==null)
+		                            	firstItemOfDir=item.FullName;
+                    		else {
+								using (var dialog = new FolderBrowserDialog())
+							    if (dialog.ShowDialog() == DialogResult.OK)
+							        firstItemOfDir = dialog.SelectedPath;
+                    		}
+	                    }
+                    
+                }
+                catch { }
+            }
+            return firstItemOfDir;//если не нашли то нулл
+        }
+
+        private void Found_writeToListExePath(string MyDirPath)
+        {
+            string[] fullFilesPath;
+
+            string[] fullDirPath = Directory.GetDirectories(MyDirPath);//забрали из папкb все пути папок с exe
+            foreach (var DirPath in fullDirPath) //перебираем все папки в папке PORT
+            {       
+                 fullFilesPath = Directory.GetFiles(DirPath, "*.exe");//ищем все exe в папке str
+                foreach(var exePath in fullFilesPath)//перебираем все exe-шки
+                    listExePath.Add(exePath);
+            }
+
+            fullFilesPath = Directory.GetFiles(MyDirPath, "*.exe");//ищем все exe в папке str
+            foreach (var exePath in fullFilesPath)//перебираем все exe-шки
+                listExePath.Add(exePath);
+        }
+
+        private void RemoveUnnecessaryExe(string MyDirPath)
+        {
+            StreamReader f = new StreamReader("RemoveExe.txt");
+            string _unExe = f.ReadToEnd();
+            _unExe=_unExe.Replace("\r",null);
+            string[] unExe = _unExe.Split('\n');int x=99999;
+            for (int k = 0; k < unExe.Length; k++)
+                for(int i=0;i<listExePath.Count;i++)
+                    if (listExePath[i] != null)
+                        if (((listExePath[i]).Equals(MyDirPath + unExe[k])))
+                            listExePath[i] = null;
+            f.Close();
+            /*x= listExePath.IndexOf(unExe[k]); не смог найти!!!- не знаю почему*/
+        }
+        //
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // StreamWriter f = new StreamWriter("HeightWidth.txt");
+            try{
+            string[] a = new string[2];           
+            a[0] = this.Height.ToString();
+            a[1] = this.Width.ToString();
+            if(a[0]=="0"||a[1]=="0") return;
+            File.WriteAllLines("HeightWidth.txt", a);   //до сюда доходит и заканчивает
+            }
+            catch{}
+            try { MousePositionThread.Abort(); }
+            catch { }//завершаем поток, иначе он сидит в процессах, хоть и закртыта прога
+        } //сохраняем размеры формы
+        //    
+        protected override void OnResize(EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.ShowInTaskbar = false;
+                this.Visible = false;
+            }
+           
+        }
+        private void Tray_MouseClick(object sender,System.Windows.Forms.MouseEventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+                {
+                    this.Visible = true;
+                    this.ShowInTaskbar = true;
+                    this.WindowState = FormWindowState.Normal;
+                }
+            else
+                {
+                    this.WindowState = FormWindowState.Minimized;
+                    this.ShowInTaskbar = false;
+                    this.Visible = false;
+                    this.Visible = true;
+                    this.ShowInTaskbar = true;
+                    this.WindowState = FormWindowState.Normal;
+
+                }
+        }
+
+    }
+}
